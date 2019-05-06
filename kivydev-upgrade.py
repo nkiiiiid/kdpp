22a53e4a9f83e84aef06575af895207acee5260b
海森堡测不准面包 <cdgeass@sina.com>
2019-05-06 18:16:26 +0800

    Add files via upload

---
+++ kivydev-upgrade.py
@@ -0,0 +1,25 @@
+#coding:u8
+
+import os
+ndk_dir = os.environ['sdk_dir']
+
+print('clean p4a dist/build/packages')
+os.system('p4a clean_all')
+
+print('install/upgrade p4a dependencies')
+os.system('sudo apt-get install -y git ant unzip virtualenv ccache autoconf libtool cmake')
+
+print('uninstall p4a')
+os.system('pip uninstall python-for-android')
+
+print('install new p4a')
+os.system('pip install git+https://github.com/kivy/python-for-android.git@master')
+
+print('download 17c ndk')
+os.system('wget -P'+' '+ndk_dir+' '+'https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip')
+
+print('unzip ndk')
+os.system('unzip'+' '+ndk_dir+'android-ndk-r17c-linux-x86_64.zip')
+
+print('upgrade complete')
+
